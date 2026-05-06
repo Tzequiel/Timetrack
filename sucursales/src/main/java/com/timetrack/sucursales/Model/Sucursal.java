75d0ec582c1186d9b36b2e0c922c1b86f126279e
Richard Hernandez <richardhernandezbarraza@gmail.com>
2026-05-05 23:31:07 -0400

    Creacion de constructores, getters y setters

--- sucursales/src/main/java/com/timetrack/sucursales/Model/Sucursal.java
+++ sucursales/src/main/java/com/timetrack/sucursales/Model/Sucursal.java
@@ -23,4 +23,69 @@ public class Sucursal {
     @Column(name = "EMPRESA_id")
     private Long empresaId;
 
+    public Sucursal(Long id, String nombre, Double latitudCentro, String direccion, Double longitudCentro, Integer radioToleranciaMetros, Long empresaId) {
+        this.id = id;
+        this.nombre = nombre;
+        this.latitudCentro = latitudCentro;
+        this.direccion = direccion;
+        this.longitudCentro = longitudCentro;
+        this.radioToleranciaMetros = radioToleranciaMetros;
+        this.empresaId = empresaId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Double getLatitudCentro() {
+        return latitudCentro;
+    }
+
+    public void setLatitudCentro(Double latitudCentro) {
+        this.latitudCentro = latitudCentro;
+    }
+
+    public Double getLongitudCentro() {
+        return longitudCentro;
+    }
+
+    public void setLongitudCentro(Double longitudCentro) {
+        this.longitudCentro = longitudCentro;
+    }
+
+    public Integer getRadioToleranciaMetros() {
+        return radioToleranciaMetros;
+    }
+
+    public void setRadioToleranciaMetros(Integer radioToleranciaMetros) {
+        this.radioToleranciaMetros = radioToleranciaMetros;
+    }
+
+    public Long getEmpresaId() {
+        return empresaId;
+    }
+
+    public void setEmpresaId(Long empresaId) {
+        this.empresaId = empresaId;
+    }
 }
