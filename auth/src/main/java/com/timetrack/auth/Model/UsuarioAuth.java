1e089cefc2aa8c8f7bdc7ab3d2bc78b62f26f790
amadorsin <acacaok@gmail.com>
2026-05-06 09:34:31 -0400

    validations

--- auth/src/main/java/com/timetrack/auth/Model/UsuarioAuth.java
+++ auth/src/main/java/com/timetrack/auth/Model/UsuarioAuth.java
@@ -1,15 +1,24 @@
 package com.timetrack.auth.Model;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "USUARIO")
 public class UsuarioAuth {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotBlank(message = "El correo electrónico es obligatorio")
+    @Email(message = "Debe ingresar un formato de correo electrónico válido")
     private String email;
-    
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener un mínimo de 8 caracteres")
     @Column(name = "password_hash")
     private String passwordHash;
 
@@ -36,5 +45,4 @@ public class UsuarioAuth {
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
     }
-    
 }
