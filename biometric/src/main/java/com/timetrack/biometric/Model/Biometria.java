97e7d2c3ccf6efe614a38b4d161546e5cb63343b
amadorsin <acacaok@gmail.com>
2026-05-06 09:39:55 -0400

    validations

--- biometric/src/main/java/com/timetrack/biometric/Model/Biometria.java
+++ biometric/src/main/java/com/timetrack/biometric/Model/Biometria.java
@@ -1,20 +1,27 @@
 package com.timetrack.biometric.Model;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "BIOMETRIA")
 public class Biometria {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "El vector facial es obligatorio y no puede estar vacío")
     @Column(name = "vector_facial")
     private String vectorFacial; 
     
+    @NotNull(message = "La fecha de registro es obligatoria")
     @Column(name = "fecha_registro")
     private LocalDateTime fechaRegistro;
     
+    @NotNull(message = "El ID del usuario es obligatorio")
     @Column(name = "USUARIO_id")
     private Long usuarioId;
 
@@ -49,6 +56,4 @@ public class Biometria {
     public void setUsuarioId(Long usuarioId) {
         this.usuarioId = usuarioId;
     }
-
-    
 }
