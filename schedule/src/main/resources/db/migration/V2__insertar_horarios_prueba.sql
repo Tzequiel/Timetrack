a831453f4db68878eca81690e80b9a4269bce1b4
amadorsin <acacaok@gmail.com>
2026-05-11 17:46:30 -0400

    flyways schedule

---
+++ schedule/src/main/resources/db/migration/V2__insertar_horarios_prueba.sql
@@ -0,0 +1,6 @@
+INSERT INTO HORARIO_TURNO (hora_entrada, hora_salida, USUARIO_id, DIA_SEMANA_id) VALUES
+('08:30', '18:30', 1, 1),
+('08:30', '18:30', 1, 2),
+('08:30', '18:30', 1, 3),
+('08:30', '18:30', 1, 4),
+('08:30', '18:30', 1, 5);
