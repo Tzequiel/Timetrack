4498f723cec4965071a3b343844931870777925b
amadorsin <acacaok@gmail.com>
2026-05-11 17:42:11 -0400

    flyways biometric

---
+++ biometric/src/main/resources/db/migration/V2__insertar_biometria_prueba.sql
@@ -0,0 +1,3 @@
+INSERT INTO BIOMETRIA (vector_facial, fecha_registro, USUARIO_id) VALUES
+('abc123xyz_hash_facial_simulado_001', '2026-05-11 08:00:00', 1),
+('def456uvw_hash_facial_simulado_002', '2026-05-11 08:30:00', 2);
