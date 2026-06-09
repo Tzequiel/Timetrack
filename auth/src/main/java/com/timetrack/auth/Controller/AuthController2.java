8f5d12f4294e1c39a60a946602838de4bfdb1635
Albedo-AB <di.aravenab@duocuc.cl>
2026-06-09 13:30:17 -0400

    authcontroller2

--- auth/src/main/java/com/timetrack/auth/Controller/AuthController2.java
+++ auth/src/main/java/com/timetrack/auth/Controller/AuthController2.java
@@ -1,4 +1,38 @@
 package com.timetrack.auth.Controller;
 
+import com.timetrack.auth.Dto.LoginJWTDTO;
+import com.timetrack.auth.Dto.ResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController2 {
+    @Autowired
+    private AuthService2 authService;
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginJWTDTO request) {
+        ResponseDTO response = authService.validar(request);
+        switch (response.getRespuestaInteger()) {
+            case 0:
+                return ResponseEntity.ok(Map.of("token", response.getRespuestaText()));
+            case 1:
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(response.getRespuestaText());
+            case 2:
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(response.getRespuestaText());
+            default:
+                break;
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("error no capturado");
+    }
 }
