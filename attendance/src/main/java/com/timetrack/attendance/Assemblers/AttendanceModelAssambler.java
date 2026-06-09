c3bb0880cc7d7124882d900e62916bd6a2dce1a7
Tzequiel <richardhernandezbarraza@gmail.com>
2026-06-09 13:36:38 -0400

    Avances implementacion assamblers

---
+++ attendance/src/main/java/com/timetrack/attendance/Assemblers/AttendanceModelAssambler.java
@@ -0,0 +1,22 @@
+package com.timetrack.attendance.Assemblers;
+
+import com.timetrack.attendance.Controller.AsistenciaController;
+import com.timetrack.attendance.Model.Asistencia;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+public class AttendanceModelAssambler implements RepresentationModelAssembler<Asistencia, EntityModel<Asistencia>> {
+    @Override
+    public EntityModel<Asistencia> toModel(Asistencia entity) {
+        // listamos las rutas GET que tenemos
+        return EntityModel.of(entity,
+                linkTo(methodOn(Asistencia.class).buscarPorId(entity.getId())).withSelfRel(),
+                linkTo(methodOn(Asistencia.class).buscarTodos()).withRel("tipos-usuario")
+        );
+    }
+}
